49ba858b094eb5d1c25eda2e6c49d75ca102b045
VNC Company <41409442+vncloudsco@users.noreply.github.com>
2021-10-19 17:21:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ function docker_install() {
 function docker_compose_install() {
     docker_compose_check=$(which docker-compose)
     if [ -z "$docker_compose_check" ]; then
+        apt install jq -y || yum install jq -y
         compose_version=$(curl https://api.github.com/repos/docker/compose/releases/latest | jq .name -r)
         output='/usr/local/bin/docker-compose'
         curl -L https://github.com/docker/compose/releases/download/$compose_version/docker-compose-$(uname -s)-$(uname -m) -o $output
